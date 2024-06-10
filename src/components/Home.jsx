a53d4a2b63fb3481831fc351d503ac587c3943af
safabeegum <pasafabeegum@gmail.com>
2024-06-11 00:01:49 +0530

    Completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="card">
+                <img src="https://img.freepik.com/free-photo/guy-shows-document-girl-group-young-freelancers-office-have-conversation-working_146671-13569.jpg?t=st=1718044117~exp=1718047717~hmac=adccf73eb44b5a933ee6ec8f653df76bfb0991c6fbe0b389bfb491c5e55a74b6&w=1060" class="card-img-top" alt="..."></img>
+  
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
